ab6bde75a75a1a4db2a7f1532d720d5acb30abd6
rabindrapatra <rabindrapatra@corp.com>
2024-12-23 02:00:59 +0530

    count sub-arrays whose sum is equal to K

---
+++ java-language-examples/src/test/java/org/dsa/examples/arrays/SubArraySumEqualsKTest.java
@@ -0,0 +1,17 @@
+package org.dsa.examples.arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubArraySumEqualsKTest {
+
+  @Test
+  public void solution() {
+    int[] nums = {2, 5, 8, 1, 5, 8};
+    int k = 13;
+    SubArraySumEqualsK t = new SubArraySumEqualsK();
+    int result = t.solution(nums, k);
+    System.out.println(result);
+  }
+}
