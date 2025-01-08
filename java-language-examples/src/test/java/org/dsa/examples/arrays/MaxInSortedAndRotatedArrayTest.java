92702373daa53694f6ad8dc3e7dee8fcc27d6169
rabindrapatra <rabindrapatra@corp.com>
2025-01-08 16:34:32 +0530

    maximum in sorted and rotated array

---
+++ java-language-examples/src/test/java/org/dsa/examples/arrays/MaxInSortedAndRotatedArrayTest.java
@@ -0,0 +1,17 @@
+package org.dsa.examples.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxInSortedAndRotatedArrayTest {
+
+  @Test
+  public void solution() {
+    int[] nums = {3, 4, 5, 1, 2};
+    MaxInSortedAndRotatedArray min = new MaxInSortedAndRotatedArray();
+    int maxValue = min.solution(nums);
+    Assert.assertSame(5, maxValue);
+  }
+}
