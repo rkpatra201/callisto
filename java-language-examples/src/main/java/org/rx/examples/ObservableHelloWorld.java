5921aab3b0c6604c4eae3bc4877e046d8665b41e
rabindrapatra <rabindrapatra@corp.com>
2025-02-28 19:30:14 +0530

    rx java streaming example

--- java-language-examples/src/main/java/org/rx/examples/ObservableHelloWorld.java
+++ java-language-examples/src/main/java/org/rx/examples/ObservableHelloWorld.java
@@ -1,10 +1,15 @@
 package org.rx.examples;
 
 import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ObservableHelloWorld {
   public static void main(String[] args) throws InterruptedException {
@@ -18,8 +23,13 @@ public class ObservableHelloWorld {
 
       @Override
       public void onNext(@NonNull String s) {
-        System.out.println("on next"+s);
+        System.out.println("on next: "+s);
         System.out.println(Thread.currentThread());
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
 
       }
 
@@ -34,15 +44,22 @@ public class ObservableHelloWorld {
       public void onComplete() {
         System.out.println("complete");
         System.out.println(Thread.currentThread());
-
       }
     };
-    Observable<String> observable = Observable.just("Hello", "RxJava");
-    observable
-        .subscribeOn(Schedulers.io())          // Runs on a background I/O thread
-        .observeOn(Schedulers.computation())
-        .subscribe(listener);
 
-    Thread.sleep(2000);
+    ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+
+    Observable.just("Hello", "World", "India")
+        .flatMap(item -> Observable.just(item)
+            .subscribeOn(Schedulers.from(executorService))  // Assign different items to different threads
+            .map(str -> {
+              System.out.println("Processing: " + str + " on " + Thread.currentThread().getName());
+              return str;
+            })
+        )
+        .subscribe(result -> System.out.println("Received: " + result + " on " + Thread.currentThread().getName()));
+
+    Thread.sleep(5000000);
   }
 }
