6480533f2228cd7fc2c9f99a1a71d6e3238ae773
rabindrapatra <rabindrapatra@corp.com>
2025-03-04 12:13:33 +0530

    diameter of tree

--- java-language-examples/src/main/java/org/dsa/examples/trees/ex1/DiameterOfTree_DFS.java
+++ java-language-examples/src/main/java/org/dsa/examples/trees/ex1/DiameterOfTree_DFS.java
@@ -2,12 +2,15 @@ package org.dsa.examples.trees.ex1;
 
 import org.dsa.examples.trees.traversal.TreeNode;
 
-public class DiameterOfTree {
+public class DiameterOfTree_DFS {
 
   int diameter = 0;
 
   public int solution(TreeNode node) {
-    if (node == null) {
+    if(node == null){
+      return 0;
+    }
+    if (node.left == null && node.right == null) { // leaf node
       return 0;
     }
     int lh = solution(node.left);
