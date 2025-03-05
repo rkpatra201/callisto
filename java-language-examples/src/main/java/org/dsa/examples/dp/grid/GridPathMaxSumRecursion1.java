0d0d80a225d8941bc63a1e29cb3d0bf3e6a637c4
rabindrapatra <rabindrapatra@corp.com>
2025-03-05 22:47:39 +0530

    grid max path sum recurssion

---
+++ java-language-examples/src/main/java/org/dsa/examples/dp/grid/GridPathMaxSumRecursion1.java
@@ -0,0 +1,38 @@
+package org.dsa.examples.dp.grid;
+
+public class GridPathMaxSumRecursion1 extends GridPathMaxSumRecursion {
+
+  /**
+   * right and down move allowed
+   *
+   * @param arr
+   * @param i
+   * @param j
+   * @param m
+   * @param n
+   * @return
+   */
+  public int solution(int[][] arr, int i, int j, int m, int n) {
+
+    solutionRecursive(arr, i, j, m, n, 0);
+
+    return maxSum;
+
+  }
+
+  private static int maxSum = Integer.MIN_VALUE;
+  public void solutionRecursive(int[][] arr, int i, int j, int m, int n, int sum) {
+
+
+    if (i == m && j == n) {
+     maxSum = Math.max(maxSum, sum + arr[i][j]);
+    }
+    if (i > m || j > n) {
+      return;
+    }
+    int currentSum = arr[i][j];
+    solutionRecursive(arr, i+1, j, m, n, sum + currentSum);
+    solutionRecursive(arr, i, j+1, m, n, sum + currentSum);
+
+  }
+}
