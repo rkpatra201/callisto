d5922d03bb2a63b55ba75fd555b030bff9ff6dda
rabindrapatra <rabindrapatra@corp.com>
2025-03-06 09:34:32 +0530

    sort with exact one swap

---
+++ java-language-examples/src/main/java/org/dsa/examples/sorting/SortWithExactOneSwap.java
@@ -0,0 +1,28 @@
+package org.dsa.examples.sorting;
+
+public class SortWithExactOneSwap {
+  public void solution(int[] nums) {
+    int j = -1, k = -1;
+
+    // Find first misplaced element
+    for (int i = 1; i < nums.length; i++) {
+      if (j == -1 && nums[i] < nums[i - 1]) {
+        j = i - 1; // First out-of-order index
+      }
+
+      if (j != -1 && nums[i] < nums[i - 1]) {
+        k = i; // Last out-of-order index
+      }
+    }
+
+    // Ensure valid swap indices
+    if (j == -1 || k == -1) {
+      return;
+    }
+
+    // Swap the misplaced elements
+    int temp = nums[j];
+    nums[j] = nums[k];
+    nums[k] = temp;
+  }
+}
