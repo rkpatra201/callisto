2692b8d8bdc9730493759fc438f468ae5f8e938e
rabindrapatra <rabindrapatra@corp.com>
2025-03-06 20:14:48 +0530

    test quick sort with duplicate values

--- java-language-examples/src/test/java/org/dsa/examples/arrays/QuickSortTest.java
+++ java-language-examples/src/test/java/org/dsa/examples/arrays/QuickSortTest.java
@@ -10,7 +10,7 @@ public class QuickSortTest {
 
   @Test
   public void solution() {
-    int[] nums = {1,2,3,8,4,7,5};//{10, 2, 30, 6, 7, 4, 5};
+    int[] nums = {1,2,5,3,8,4,7,5};//{10, 2, 30, 6, 7, 4, 5};
     QuickSort sort = new QuickSort();
     sort.solution(nums);
     System.out.println(Arrays.toString(nums));
