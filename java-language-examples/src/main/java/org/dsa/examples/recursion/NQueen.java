554020b612b32b81ff6e51ec577be36fb2b54fc4
rabindrapatra <rabindrapatra@corp.com>
2025-03-08 11:04:25 +0530

    solve nqueen one solution with return | all solution with backtrack

--- java-language-examples/src/main/java/org/dsa/examples/recursion/NQueen.java
+++ java-language-examples/src/main/java/org/dsa/examples/recursion/NQueen.java
@@ -7,7 +7,7 @@ import java.util.List;
 // backtracking
 public class NQueen {
 
-  private static class Position{
+  private static class Position {
     int row;
     int col;
 
@@ -25,45 +25,45 @@ public class NQueen {
     }
   }
 
-  public List<Position> solution(int n){
+  public List<Position> solution(int n) {
     List<Position> positions = new ArrayList<>();
-       solveNQueenCol(n, 0, positions);
-  //  System.out.println(positions);
+    solveNQueenCol(n, 0, positions);
+    //  System.out.println(positions);
     return positions;
   }
 
-  private boolean isUnderAttack(List<Position> positions, int row, int col){
-    for(Position p: positions){
-      if(p.row == row){ // check same row
+  private boolean isUnderAttack(List<Position> positions, int row, int col) {
+    for (Position p : positions) {
+      if (p.row == row) { // check same row
         return true;
       }
-      if(p.col == col){ // check same column
+      if (p.col == col) { // check same column
         return true;
       }
-      if(Math.abs(p.row -p.col) == Math.abs(row-col)){ // check same diagonal: tricky
+      if (Math.abs(p.row - row) == Math.abs(p.col - col)) { // check same diagonal: tricky
         return true;
       }
     }
     return false;
   }
 
-  private boolean solveNQueenRow(int n, int col, List<Position> positionList){
+  private boolean solveNQueenRow(int n, int col, List<Position> positionList) {
 
-    if(col == n){
+    if (col == n) {
       System.out.println(positionList);
       return true;
     }
 
-    for(int row=0; row < n; row++){
+    for (int row = 0; row < n; row++) {
 
-      if(isUnderAttack(positionList, row, col)){
+      if (isUnderAttack(positionList, row, col)) {
         continue;
       }
 
       Position p = new Position(row, col);
       positionList.add(p);
       boolean flag = solveNQueenRow(n, col + 1, positionList);
-      if(flag){
+      if (flag) {
         return true;
       }
       positionList.remove(p);
@@ -72,24 +72,24 @@ public class NQueen {
   }
 
 
-  private boolean solveNQueenCol(int n, int row, List<Position> positionList){
+  private boolean solveNQueenCol(int n, int row, List<Position> positionList) {
 
-    if(row == n){
+    if (row == n) {
       System.out.println(positionList);
       return true;
     }
 
-    for(int col=0; col < n; col++){
+    for (int col = 0; col < n; col++) {
 
-      if(isUnderAttack(positionList, row, col)){
+      if (isUnderAttack(positionList, row, col)) {
         continue;
       }
 
       Position p = new Position(row, col);
       positionList.add(p);
       boolean flag = solveNQueenCol(n, row + 1, positionList);
-      if(flag){
-        return true;
+      if (flag) {
+        return true; // as we want to find one solution, so return from here without backtrack
       }
       positionList.remove(p);
     }
@@ -97,16 +97,16 @@ public class NQueen {
   }
 
 
-  private void solveNQueenAllSolns(int n, int row, List<Position> positionList){
+  public void solveNQueenAllSolns(int n, int row, List<Position> positionList) {
 
-    if(row == n){
+    if (row == n) {
       System.out.println(positionList);
 //      return true;
     }
 
-    for(int col=0; col < n; col++){
+    for (int col = 0; col < n; col++) {
 
-      if(isUnderAttack(positionList, row, col)){
+      if (isUnderAttack(positionList, row, col)) {
         continue;
       }
 
@@ -117,6 +117,10 @@ public class NQueen {
     }
 
 //    return false;
+  }
+
+  public void solveNQueenAllSolns(int n) {
+    solveNQueenAllSolns(n, 0, new ArrayList<>());
   }
 
 }
