944686770a367a8414af07e09e1e174f7e94d598
rabindrapatra <rabindrapatra@corp.com>
2025-03-18 09:06:02 +0530

    max path sum java

--- java-language-examples/src/main/java/org/dsa/examples/trees/ex1/MaxPathSum_DFS.java
+++ java-language-examples/src/main/java/org/dsa/examples/trees/ex1/MaxPathSum_DFS.java
@@ -6,6 +6,7 @@ import org.dsa.examples.trees.traversal.TreeNode;
  * The Maximum Path Sum in a binary tree problem involves finding the maximum sum of values along any path in the tree.
  * A path can start and end at any node but must follow parent-child connections.
  */
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/submissions/1577456217/
 public class MaxPathSum_DFS {
 
   public static int solution(TreeNode treeNode) {
@@ -15,18 +16,17 @@ public class MaxPathSum_DFS {
 
   private static int maxSum = Integer.MIN_VALUE;
 
-  private static int traverse(TreeNode root) {
-    if (root == null) {
+  public static int traverse(TreeNode root){
+    if(root == null){
       return 0;
     }
 
-    int leftSum = traverse(root.left);
-    int rightSum = traverse(root.right);
+    int l = Math.max(0, traverse(root.left));
+    int r = Math.max(0, traverse(root.right));
 
-    maxSum = Math.max(maxSum, leftSum + rightSum + root.data);
+    maxSum = Math.max(maxSum, l+r+root.data);
 
-    return Math.max(leftSum, rightSum) + root.data;
-
+    return Math.max(l,r) + root.data;
   }
 
 }
