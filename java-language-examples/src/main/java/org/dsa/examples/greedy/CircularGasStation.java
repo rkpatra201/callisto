1a48109fa7f496443af32e20c55e7771a2a970eb
rabindrapatra <rabindrapatra@corp.com>
2025-03-19 09:07:04 +0530

    circular gas station can complete circuit

---
+++ java-language-examples/src/main/java/org/dsa/examples/greedy/CircularGasStation.java
@@ -0,0 +1,32 @@
+package org.dsa.examples.greedy;
+
+// https://www.youtube.com/watch?v=lJwbPZGo05A
+// https://leetcode.com/problems/gas-station/submissions/1578606124/
+public class CircularGasStation {
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    if (sum(gas) < sum(cost)) {
+      return -1;
+    }
+
+    int total = 0;
+    int index = 0;
+
+    for (int i = 0; i < gas.length; i++) {
+      total += (gas[i] - cost[i]);
+      if (total < 0) {
+        total = 0; // reset and try from next index
+        index = i + 1;
+      }
+    }
+    return index;
+  }
+
+  private int sum(int[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
+  }
+
+}
