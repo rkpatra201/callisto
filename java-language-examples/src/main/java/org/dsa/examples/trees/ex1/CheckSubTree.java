df51f066ea20a8d64ed424077355258f9aff9209
rabindrapatra <rabindrapatra@corp.com>
2025-03-21 22:09:42 +0530

    check for subtree

--- java-language-examples/src/main/java/org/dsa/examples/trees/ex1/CheckSubTree.java
+++ java-language-examples/src/main/java/org/dsa/examples/trees/ex1/CheckSubTree.java
@@ -2,11 +2,11 @@ package org.dsa.examples.trees.ex1;
 
 import org.dsa.examples.trees.traversal.TreeNode;
 
+// https://leetcode.com/problems/subtree-of-another-tree/submissions/1581509646/
 public class CheckSubTree {
 
   public boolean solution(TreeNode source, TreeNode target){
-    if(source == null) return true;
-    if(target == null) return false;
+    if(source == null) return false; // a null source cant contain a target tree. so return false
 
     boolean result = new CheckSameTree_DFS().solution(source, target);
     if(result) return result;
