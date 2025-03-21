e40e30575e7e0b519baa8e248844385c39dc9606
rabindrapatra <rabindrapatra@corp.com>
2025-03-22 01:06:59 +0530

    dsa refactor

--- java-language-examples/src/main/java/org/dsa/examples/strings/StringPermutation.java
+++ java-language-examples/src/main/java/org/dsa/examples/strings/StringPermutation.java
@@ -5,22 +5,23 @@ import java.util.List;
 
 public class StringPermutation {
 
-    public List<String> solution(String input){
-        List<String> result = new ArrayList<>();
-        recursive(result, input, "");
-        return result;
+  public List<String> solution(String input) {
+    List<String> result = new ArrayList<>();
+    recursive(result, input, "");
+    return result;
+  }
+
+  private void recursive(List<String> result, String input, String output) {
+    if (input.isEmpty()) {
+      result.add(output);
+      return;
     }
 
-    private void recursive(List<String> result, String input, String output){
-      if(input.isEmpty()){
-        result.add(output);
-        return;
-      }
-
-      for(int i = 0; i < input.length();  i++){
-        String firstPart = input.substring(0, i);
-        String secondPart = input.substring(i);
-        recursive(result, firstPart, output+secondPart);
-      }
+    for (int i = 0; i < input.length(); i++) {
+      System.out.println(i);
+      String firstPart = input.substring(0, i);
+      String secondPart = input.substring(i + 1);
+      recursive(result, firstPart + secondPart, output + input.charAt(i));
     }
+  }
 }
