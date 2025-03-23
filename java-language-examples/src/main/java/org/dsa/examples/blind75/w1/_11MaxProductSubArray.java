e3cbbdbf0d041b37a488cca4d8a13e8db6bc267a
rabindrapatra <rabindrapatra@corp.com>
2025-03-24 00:17:42 +0530

    max product sub array

---
+++ java-language-examples/src/main/java/org/dsa/examples/blind75/w1/_11MaxProductSubArray.java
@@ -0,0 +1,35 @@
+package org.dsa.examples.blind75.w1;
+
+
+// https://leetcode.com/problems/maximum-product-subarray/submissions/1583607189/
+public class _11MaxProductSubArray {
+  public int maxProduct(int[] nums) {
+    int max = 1;
+    int min = 1;
+    int result = nums[0];
+    for (int i = 0; i < nums.length; i++) {
+      int curr = nums[i];
+      if (curr == 0) {
+        max = 1;
+        min = 1;
+        result = Math.max(result, 0);
+        continue;
+      }
+
+      int curr_max = curr * max;
+      int curr_min = curr * min;
+
+      int t_min = Math.min(curr, Math.min(curr_max, curr_min));
+      int t_max = Math.max(curr, Math.max(curr_max, curr_min));
+
+      int t_result = Math.max(result, Math.max(t_max, t_min));
+
+      result = t_result;
+
+      min = t_min;
+      max = t_max;
+    }
+
+    return result;
+  }
+}
